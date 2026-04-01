dcc8f5da952ca86fe8fa6b258db82959a9af9ee0
hoang.nguyencong <hoang.nguyencong@cybereye.vn>
2026-04-01 15:36:08 +0700

    update code

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::post('/admin/logout', [RsvpController::class, 'adminLogout'])->name('admi
 Route::delete('/admin/rsvp/{rsvp}/wish', [RsvpController::class, 'deleteWish'])->name('admin.wish.delete');
 Route::get('/admin/config', [RsvpController::class, 'adminConfig'])->middleware('throttle.perip')->name('admin.config');
 Route::post('/admin/config', [RsvpController::class, 'updateConfig'])->middleware('throttle.perip')->name('admin.config.update');
-Route::get('/admin/gallery', [RsvpController::class, 'adminGallery'])->middleware('throttle.perip')->name('admin.gallery');
-Route::post('/admin/gallery', [RsvpController::class, 'storeGalleryPhoto'])->middleware('throttle.perip')->name('admin.gallery.store');
+Route::get('/admin/gallery', [RsvpController::class, 'adminGallery'])->name('admin.gallery');
+Route::post('/admin/gallery', [RsvpController::class, 'storeGalleryPhoto'])->name('admin.gallery.store');
 Route::put('/admin/gallery/{photo}', [RsvpController::class, 'updateGalleryPhoto'])->name('admin.gallery.update');
 Route::delete('/admin/gallery/{photo}', [RsvpController::class, 'deleteGalleryPhoto'])->name('admin.gallery.delete');
